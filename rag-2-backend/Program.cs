28c7ae73f51caa5443f1c71879c5e8f469b073f9
Marcin Bator <marcinbator.ofc@gmail.com>
2024-07-02 18:27:18 +0200

    feat: #main database connection

--- rag-2-backend/Program.cs
+++ rag-2-backend/Program.cs
@@ -1,11 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using rag_2_backend.data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
+
+var configuration = builder.Configuration;
+
+builder.Services.AddDbContext<UserContext>(options =>
+    options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
@@ -18,8 +25,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
+

ee470d6cc76150ae006346fee387b088a9b60c8e
Marcin Bator <marcinbator.ofc@gmail.com>
2024-11-11 18:25:41 +0100

    feat: #dev Paweu in migrations

--- rag-2-backend/Migrations/20241107192532_Init.cs
+++ rag-2-backend/Migrations/20241107192532_Init.cs
@@ -17,6 +17,13 @@ namespace rag_2_backend.Migrations
                 VALUES ('173592@stud.prz.edu.pl', '$2a$11$gCjVX/DIAkLC4C3Qt814d.mg.vsp1cFjsX67cuvfzQBjuhy4K7LIW', 
                         'Marcin', 2, true, 2022, 2023, false, '-infinity')
                 ON CONFLICT DO NOTHING;
+
+                INSERT INTO public.user_table 
+                    (""Email"", ""Password"", ""Name"", ""Role"", ""Confirmed"", ""StudyCycleYearA"", 
+                        ""StudyCycleYearB"", ""Banned"", ""LastPlayed"")
+                VALUES ('173599@stud.prz.edu.pl', '$2a$11$dZOcaszbVfd2B3fMUPC/jOlmk2RiO0fALub8H7sQzDRUjN2WlImrm', 
+                        'Paweł', 2, true, 2022, 2023, false, '-infinity')
+                ON CONFLICT DO NOTHING;
             ");
 
             migrationBuilder.Sql(@"
